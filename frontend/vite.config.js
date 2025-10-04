72c9c18740b567010050b80a5589098514d5da52
Muciu <constantinmucioniu@gmail.com>
2025-10-04 15:07:39 +0300

    Docker containerization added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /*server: {
-    proxy: {
-      // Redirectează toate request-ile care încep cu "/api" către backend
-      "/api": {
-        target: "http://localhost:5000", // Adresa backend-ului
-        changeOrigin: true,
-        secure: false,
-        // Rescrie calea dacă backend-ul așteaptă un URL fără "/api"
-        // rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+  server: {
+    host: "0.0.0.0", // Ascultă pe toate interfețele
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true, // Pentru hot-reload în Docker
     },
-  },*/
+  },
 });
